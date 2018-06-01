2213ac98ea34f55b7eddf683d8a58ba6fbabc757
Alex.Xiang <xiangbohua@hotmail.com>
2018-06-01 21:51:00 +0800

    remove some print

--- tool.py
+++ tool.py
@@ -34,8 +34,6 @@ def downloadImg(imageUrl, savePath):
             f = open(savePath, 'wb')  # 若是'wb'就表示写二进制文件
             f.write(zipData)
             f.close()
-    else:
-        print('图片已存在:'+ savePath)
 
 def read_gz_file(path):
     '''read the existing gzip-format file,and return the content of this file'''
